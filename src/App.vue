c0f7e64aa2e2eb5d12e2f6e4173863f50d6c9d78
Jérémy <frazierjeremi@gmail.com>
2024-06-09 12:17:38 +0200

    test image header sans lien

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
         </div>
         <ul :class="{'nav-links': true, 'open': isMenuOpen}">
           <li class="imgheader">
-            <RouterLink to="/"><img src="../../public/images/Logo_PR.png" alt="Logo"></RouterLink>
+            <img src="../../public/images/Logo_PR.png" alt="Logo">
           </li>
           <li class="homeheader"><RouterLink to="/">Accueil</RouterLink></li>
           <li class="enterpriseheader"><RouterLink to="/entreprise">L'entreprise</RouterLink></li>
