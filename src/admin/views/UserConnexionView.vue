14932a2cf884ce8535177066287c08f369043320
Jérémy <frazierjeremi@gmail.com>
2024-06-12 00:44:59 +0200

    correct beug

--- src/admin/views/UserConnexionView.vue
+++ src/admin/views/UserConnexionView.vue
@@ -13,19 +13,32 @@
     data() {
       return {
         username: '',
-        password: ''
+        password: ''  
       };
     },
     methods: {
       async login() {
-        try {
-          const response = await axios.post('http://localhost:5000/api/auth/login', {
-            user: this.username,
-            password: this.password
-          });
-  
+      console.log(this.username);
+      console.log(this.password)
+          try {
+    const response = await fetch('http://localhost:5000/api/auth/login', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        user: this.username,
+        password: this.password 
+      })
+    });
+    if (!response.ok) {
+      throw new Error("Failed to log user");
+    }
+  console.log(response)
+  const API_RESPONSE = await response.json()
+  console.log(API_RESPONSE)
           // Stocker le token dans localStorage
-          localStorage.setItem('token', response.data.token);
+          localStorage.setItem('token', API_RESPONSE.token);
   
           // Rediriger l'utilisateur ou faire autre chose après la connexion réussie
           this.$router.push('/admin/drone');
